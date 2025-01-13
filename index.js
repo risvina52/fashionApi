0d9917bc4d5690857706366402a04234e144e825
risvina52 <risvina52@gmail.com>
2025-01-14 06:01:55 +0700

    update first

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const data = [
+    { id: 1, name: "John Doe", age: 30 },
+    { id: 2, name: "Jane Doe", age: 25 },
+];
+
+// Endpoint GET
+app.get('/api/users', (req, res) => {
+    res.json(data);
+});
+
+// Endpoint POST
+app.post('/api/users', (req, res) => {
+    const newUser = req.body;
+    data.push(newUser);
+    res.status(201).json(newUser);
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
